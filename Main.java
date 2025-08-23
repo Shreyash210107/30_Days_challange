f96a606e68d3463efcfaba19f816309981bc124d
Shreyash210107 <patilshreyash210107@gmail.com>
2025-08-23 22:41:53 +0530

    Add files via upload
    ok

---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main
+{
+ public static void main(String[] args)
+ { 
+   String[] strs = {"flower","flow","flight"};
+   
+     if(strs.length == 0)
+     {
+       System.out.println("");
+       return;
+     }
+
+     String prefix = strs[0];
+     for(int i = 1; i< strs.length;i++)
+     {
+       while(! strs[i].startsWith(prefix))
+       {
+         prefix = prefix.substring(0,prefix.length() - 1);
+           if(prefix.isEmpty())
+           {
+            System.out.println("");
+                              return;
+           } 
+        }  
+     }
+    System.out.println(prefix);
+  }
+}
